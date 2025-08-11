d17e6528af90220680d9ffe51c5a5d2ecddda690
Durga Prasad <saikamireddi3@gmail.com>
2025-08-11 20:24:26 +0530

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import "./App.css";
 import OtpInput from './components/OtpInput.jsx';
+import Login from './pages/Login.jsx';
 import SignUp from './pages/SignUp.jsx';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <OtpInput size={4} onSubmit={(otp) => console.log(otp)}></OtpInput>
       <SignUp />
+      <Login />
     </>
   );
 }
