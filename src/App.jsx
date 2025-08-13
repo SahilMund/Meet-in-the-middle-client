18a8f78033c51ee5f5079791ac59ab1a383a1e5e
Sahebzad Kushal Deep <kushaldeep448@gmail.com>
2025-08-13 15:22:21 +0530

    Landing page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import "./App.css";
 import OtpInput from './components/OtpInput.jsx';
+import Landingpage from './pages/Landingpage.jsx';
 import Login from './pages/Login.jsx';
 import SignUp from './pages/SignUp.jsx';
 
@@ -9,6 +10,7 @@ function App() {
       <OtpInput size={4} onSubmit={(otp) => console.log(otp)}></OtpInput>
       <SignUp />
       <Login />
+      <Landingpage/>
     </>
   );
 }
---
+++ src/components/landingpage-components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import locationimage from '../../assets/locationimage.png'
+// src\assets\locationimage.png
+const Footer = () => {
+  return (
+    
+    <div className="flex flex-wrap items-center justify-between bg-[#FF4C61] px-6 md:px-40 py-6 gap-6">
+  {/* Left section */}
+  <div className="flex items-center gap-3">
+    <img src={locationimage} height={40} width={40} alt="logo" />
+    <h1 className="text-xl md:text-3xl font-semibold">Meet in Middle</h1>
+  </div>
+
+  {/* Right section */}
+  <h2 className="text-sm md:text-base">
+    ©Meet in Middle. All rights reserved.
+  </h2>
+</div>
+
+  )
+}
+
+export default Footer
