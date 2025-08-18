6ed2a5c29ab0abcfa350076bcfaf546ed7c84c31
Vishnu <bandarivishnu143@gmail.com>
2025-08-18 20:29:07 +0530

    profileSettingPage meetingHistory statistics meetingCard profileUpdateForm  all these components are added

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@
 import OtpInput from './components/OtpInput.jsx';
 import Landingpage from './pages/Landingpage.jsx';
 import Login from './pages/Login.jsx';
+import ProfileSettingsPage from './pages/ProfileSettingsPage.jsx';
 import SignUp from './pages/SignUp.jsx';
 
 function App() {
   return (
     <>
-      <OtpInput size={4} onSubmit={(otp) => console.log(otp)}></OtpInput>
+      {/* <OtpInput size={4} onSubmit={(otp) => console.log(otp)}></OtpInput>
       <SignUp />
       <Login />
-      <Landingpage/>
+      <Landingpage/> */}
+      <ProfileSettingsPage/>
     </>
   );
 }
