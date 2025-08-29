0aab905d529e58c285f99e9346971dcf18bec3b1
Durga Prasad <saikamireddi3@gmail.com>
2025-08-29 13:03:04 +0530

    conflicts

--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -1,14 +1,18 @@
 const endPoints = {
-    LOGIN_USER: "/user/login",
-    UPLOAD_AVATAR: "/user/uploadAvatar",
-    GET_USER_DETAILS:'/user/currUserInfo',
-    GET_OTP:'/verification/sendOtp',
-    VERIFY_OTP:'/verification/verifyOTP',
-    LOGOUT_USER:'/user/logout',
-    CREATE_MEETING:'/meeting/createMeeting',
-    GET_DASHBOARD_STATS:'/meeting/getDashBoardStats',
-    GET_ALL_MY_MEETINGS:({pageNo,items})=>'/meeting/getMeetings'+`?pageNo=${pageNo}&items=${items}`,
-    GET_UPCOMING_MEETINGS:({pageNo,items})=>'/meeting/getUpCommingMeetings'+`?pageNo=${pageNo}&items=${items}`,
-    GET_PENDING_MEETINGS:({pageNo,items})=>'/meeting/getPendingMeetings'+`?pageNo=${pageNo}&items=${items}`,
-}
+  LOGIN_USER: "/user/login",
+  UPLOAD_AVATAR: "/user/uploadAvatar",
+  GET_USER_DETAILS: "/user/currUserInfo",
+  GET_OTP: "/verification/sendOtp",
+  VERIFY_OTP: "/verification/verifyOTP",
+  LOGOUT_USER: "/user/logout",
+  CREATE_MEETING: "/meeting/createMeeting",
+  GET_DASHBOARD_STATS: "/meeting/getDashBoardStats",
+  GET_ALL_MY_MEETINGS: ({ pageNo, items }) =>
+    "/meeting/getMeetings" + `?pageNo=${pageNo}&items=${items}`,
+  GET_UPCOMING_MEETINGS: ({ pageNo, items }) =>
+    "/meeting/getUpCommingMeetings" + `?pageNo=${pageNo}&items=${items}`,
+  GET_PENDING_MEETINGS: ({ pageNo, items }) =>
+    "/meeting/getPendingMeetings" + `?pageNo=${pageNo}&items=${items}`,
+  GET_CONFLICTS: (id) => `/meeting/conflicts/${id}`,
+};
 export default endPoints;
